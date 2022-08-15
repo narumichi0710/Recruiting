c32d1c3a280ee5f9d0b6d442958950acf0ef6f01
narumichi0710 <narumichi7373@gmail.com>
2022-08-16 02:21:32 +0900

    募集詳細APIリクエストを追加

--- Recruiting/DataFlow/RecruitmentClient.swift
+++ Recruiting/DataFlow/RecruitmentClient.swift
@@ -12,32 +12,59 @@ import ComposableArchitecture
 
 // MARK: 募集機能APIClient
 struct RecruitmentClient {
-    var recruitments: (RecruitmentRequest) -> Effect<Recruitment.List, APIError>
+    var list: (RecruitmentRequest.ListRequest) -> Effect<RecruitmentModel.List, APIError>
+    var detail: (RecruitmentRequest.DetailRequest) -> Effect<RecruitmentModel.Detail, APIError>
 }
 
 extension RecruitmentClient {
-    static let live = RecruitmentClient(recruitments: { request in
-        request.publisher
-            .receive(on: request.scheduler)
-            .eraseToEffect()
-    })
+    static let live = RecruitmentClient(
+        list: { request in
+            request.publisher
+                .receive(on: request.scheduler)
+                .eraseToEffect()
+        },
+        detail: { request in
+            request.publisher
+                .receive(on: request.scheduler)
+                .eraseToEffect()
+        }
+    )
 }
 
-struct RecruitmentRequest: APIRequest {
-    /// 検索ワード
-    let searchWord: String
-    /// ページネーションナンバー
-    let pageNo: Int
-    /// スケジューラー
-    var method: HTTPMethodType { .get }
-    /// パス
-    var path: String { "projects/" }
-    /// クエリパラメータ
-    var queryParameters: [String : String] {
-        ["q": searchWord, "page": "\(pageNo)"]
+
+enum RecruitmentRequest {
+
+    // MARK: 募集一覧APIリクエスト
+    struct ListRequest: APIRequest {
+        /// 検索ワード
+        let searchWord: String
+        /// ページネーションナンバー
+        let pageNo: Int
+        /// スケジューラー
+        var method: HTTPMethodType { .get }
+        /// パス
+        var path: String { "projects/" }
+        /// クエリパラメータ
+        var queryParameters: [String : String] {
+            ["q": searchWord, "page": "\(pageNo)"]
+        }
+        /// パブリッシャー
+        var publisher: AnyPublisher<RecruitmentModel.List, APIError> {
+            request()
+        }
     }
-    /// パブリッシャー
-    var publisher: AnyPublisher<Recruitment.List, APIError> {
-        request()
+
+    // MARK: 募集詳細APIリクエスト
+    struct DetailRequest: APIRequest {
+        /// 募集ID
+        var id: Int
+        /// スケジューラー
+        var method: HTTPMethodType { .get }
+        /// パス
+        var path: String { "projects/\(id)" }
+        /// パブリッシャー
+        var publisher: AnyPublisher<RecruitmentModel.Detail, APIError> {
+            request()
+        }
     }
 }
--- Recruiting/View/Recruitment/DataFlow/RecruitmentStore.swift
+++ Recruiting/View/Recruitment/DataFlow/RecruitmentStore.swift
@@ -15,24 +15,30 @@ enum RecruitmentStore {
             /// 検索ワード.
             var searchWord: String = ""
             /// 募集一覧.
-            var recruitments: Recruitment.List? = nil
+            var recruitments: RecruitmentModel.List? = nil
+            /// 募集詳細.
+            var recruitmentsDetail: RecruitmentModel.Detail? = nil
             /// 選択中の募集.
-            var selectedCell: Selection = Selection<Recruitment.Cell>(nil)
+            var selectedCell: Selection = Selection<RecruitmentModel.Cell>(nil)
         }
         enum Action: Equatable {
-            /// レスポンス.
-            case response(Result<Recruitment.List, APIError>)
+            /// 募集一覧レスポンス.
+            case responseRecruitments(Result<RecruitmentModel.List, APIError>)
+            /// 募集詳細レスポンス.
+            case responseRecruitmentDetail(Result<RecruitmentModel.Detail, APIError>)
             /// 募集一覧取得アクション.
             case getRecruitments
+            /// 募集詳細取得アクション.
+            case getRecruitmentDetail
             /// 検索ワード変更アクション.
             case changedSearchWord(String)
             /// 募集詳細画面アクション.
-            case presentRecDetail(Recruitment.Cell?)
+            case presentRecDetail(RecruitmentModel.Cell?)
         }
 
     static let reducer = Reducer<State, Action, AppStore.Environment> { state, action, env in
             switch action {
-            case .response(let result):
+            case .responseRecruitments(let result):
                 // 募集一覧取得レスポンス.
                 switch result {
                 case .success(let response):
@@ -42,13 +48,34 @@ enum RecruitmentStore {
                     // TODO:
                     return .none
                 }
-
+            case .responseRecruitmentDetail(let result):
+                // 募集詳細取得レスポンス.
+                switch result {
+                case .success(let response):
+                    state.recruitmentsDetail = response
+                    return .none
+                case .failure(let errorResponse):
+                    // TODO:
+                    return .none
+                }
             case .getRecruitments:
                 // 募集一覧取得処理.
-                return env.recruitmentClient.recruitments(
+                return env.recruitmentClient.list(
                     .init(searchWord: state.searchWord, pageNo: 1)
                 )
-                .catchToEffect(Action.response)
+                .catchToEffect(Action.responseRecruitments)
+                .cancellable(id: State.CancelID())
+
+            case .getRecruitmentDetail:
+                guard let cellId = state.selectedCell.selectdItem?.id else {
+                    // TODO:
+                    return .none
+                }
+                // 募集詳細取得処理.
+                return env.recruitmentClient.detail(
+                    .init(id: cellId)
+                )
+                .catchToEffect(Action.responseRecruitmentDetail)
                 .cancellable(id: State.CancelID())
 
             case .changedSearchWord(let value):
@@ -59,9 +86,9 @@ enum RecruitmentStore {
             case .presentRecDetail(let selectedCell):
                 // 募集詳細画面遷移処理.
                 if selectedCell == nil {
-                    state.selectedCell = Selection<Recruitment.Cell>(nil)
+                    state.selectedCell = Selection<RecruitmentModel.Cell>(nil)
                 } else {
-                    state.selectedCell = Selection<Recruitment.Cell>(selectedCell)
+                    state.selectedCell = Selection<RecruitmentModel.Cell>(selectedCell)
                 }
                 return .none
             }
