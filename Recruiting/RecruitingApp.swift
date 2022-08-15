e517197fa7e83f2caade263d997b7bc6af5a20c3
narumichi0710 <narumichi7373@gmail.com>
2022-08-15 20:14:54 +0900

    init

---
+++ Recruiting/RecruitingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecruitingApp.swift
+//  Recruiting
+//
+//  Created by Regolith on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct RecruitingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
