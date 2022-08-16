85b3588065005d1cf98220c090ca55b09f3ba3a2
narumichi0710 <narumichi7373@gmail.com>
2022-08-17 01:31:02 +0900

    テストコードの軽微な修正

--- RecruitingTests/RecruitingTests.swift
+++ RecruitingTests/RecruitingTests.swift
@@ -5,10 +5,10 @@ import XCTest
 @testable import Recruiting
 import ComposableArchitecture
 
-
+// MARK: 募集機能テストデータ
 class RecruitingTests: XCTestCase {
 
-    // MARK: 募集一覧レスポンス成功テスト
+    // MARK: 募集一覧API成功テスト
     func testSucceedRecruitmentContents() {
         // テスト用の依存データ
         let store = TestStore(
@@ -30,7 +30,7 @@ class RecruitingTests: XCTestCase {
         )
         // 募集一覧取得アクション
         store.send(.getRecruitments)
-        // 受け取るアクション
+        // 募集一覧取得アクションによって発生するアクション
         store.receive(.responseRecruitments(.success(RecruitmentModel.mockList))) { response in
             response.recruitments = RecruitmentModel.mockList
             response.errorStatus = Bindable<String>(nil)
@@ -64,72 +64,70 @@ class RecruitingTests: XCTestCase {
             response.recruitments = nil
             response.errorStatus = Bindable<String>(APIError.serverError.localize)
         }
+    }
 
-
-        // MARK: 募集詳細レスポンス成功テスト
-        func testSucceedRecruitmentDetail() {
-            // テスト用の依存データ
-            let store = TestStore(
-                initialState: RecruitmentStore.State(),
-                reducer: RecruitmentStore.reducer,
-                environment: AppStore.Environment(
-                    recruitmentClient: RecruitmentClient(
-                        list: { request in
-                            Effect(value: RecruitmentModel.mockList)
-                                .receive(on: DispatchQueue.immediate.eraseToAnyScheduler())
-                                .eraseToEffect()
-                        },
-                        detail: { request in
-                            Effect(value: RecruitmentModel.mockDetail)
-                                .receive(on: DispatchQueue.immediate.eraseToAnyScheduler())
-                                .eraseToEffect()
-                        })
-                )
+    // MARK: 募集詳細レスポンス成功テスト
+    func testSucceedRecruitmentDetail() {
+        // テスト用の依存データ
+        let store = TestStore(
+            initialState: RecruitmentStore.State(),
+            reducer: RecruitmentStore.reducer,
+            environment: AppStore.Environment(
+                recruitmentClient: RecruitmentClient(
+                    list: { request in
+                        Effect(value: RecruitmentModel.mockList)
+                            .receive(on: DispatchQueue.immediate.eraseToAnyScheduler())
+                            .eraseToEffect()
+                    },
+                    detail: { request in
+                        Effect(value: RecruitmentModel.mockDetail)
+                            .receive(on: DispatchQueue.immediate.eraseToAnyScheduler())
+                            .eraseToEffect()
+                    })
             )
-            // セル選択アクション
-            store.send(.presentRecDetail(RecruitmentModel.mockCell)) { store in
-                store.selectedCell = Bindable<RecruitmentModel.Cell>(RecruitmentModel.mockCell)
-            }
-            // 募集詳細取得アクション
-            store.send(.getRecruitmentDetail)
-            // 受け取るアクション
-            store.receive(.responseRecruitmentDetail(.success(RecruitmentModel.mockDetail))) { response in
-                response.recruitmentsDetail = RecruitmentModel.mockDetail
-                response.errorStatus = Bindable<String>(nil)
-            }
+        )
+        // セル選択アクション
+        store.send(.presentRecDetail(RecruitmentModel.mockCell_1)) { store in
+            store.selectedCell = Bindable<RecruitmentModel.Cell>(RecruitmentModel.mockCell_1)
         }
-
-        // MARK: 募集詳細レスポンス失敗テスト
-        func testFairureRecruitmentDetail() {
-            // テスト用の依存データ
-            let store = TestStore(
-                initialState: RecruitmentStore.State(),
-                reducer: RecruitmentStore.reducer,
-                environment: AppStore.Environment(
-                    recruitmentClient: RecruitmentClient(
-                        list: { request in
-                            Effect(error: .serverError)
-                                .receive(on: DispatchQueue.immediate.eraseToAnyScheduler())
-                                .eraseToEffect()
-                        },
-                        detail: { request in
-                            Effect(error: .serverError)
-                                .receive(on: DispatchQueue.immediate.eraseToAnyScheduler())
-                                .eraseToEffect()
-                        })
-                )
-            )
-            // セル選択アクション
-            store.send(.presentRecDetail(RecruitmentModel.mockCell)) { store in
-                store.selectedCell = Bindable<RecruitmentModel.Cell>(RecruitmentModel.mockCell)
-            }
-            // 募集詳細取得アクション
-            store.send(.getRecruitmentDetail)
-            // 受け取るアクション
-            store.receive(.responseRecruitmentDetail(.failure(APIError.serverError))) { response in
-                response.errorStatus = Bindable<String>(APIError.serverError.localize)
-            }
+        // 募集詳細取得アクション
+        store.send(.getRecruitmentDetail)
+        // 受け取るアクション
+        store.receive(.responseRecruitmentDetail(.success(RecruitmentModel.mockDetail))) { response in
+            response.recruitmentsDetail = RecruitmentModel.mockDetail
+            response.errorStatus = Bindable<String>(nil)
         }
     }
 
+    // MARK: 募集詳細レスポンス失敗テスト
+    func testFairureRecruitmentDetail() {
+        // テスト用の依存データ
+        let store = TestStore(
+            initialState: RecruitmentStore.State(),
+            reducer: RecruitmentStore.reducer,
+            environment: AppStore.Environment(
+                recruitmentClient: RecruitmentClient(
+                    list: { request in
+                        Effect(error: .serverError)
+                            .receive(on: DispatchQueue.immediate.eraseToAnyScheduler())
+                            .eraseToEffect()
+                    },
+                    detail: { request in
+                        Effect(error: .serverError)
+                            .receive(on: DispatchQueue.immediate.eraseToAnyScheduler())
+                            .eraseToEffect()
+                    })
+            )
+        )
+        // セル選択アクション
+        store.send(.presentRecDetail(RecruitmentModel.mockCell_1)) { store in
+            store.selectedCell = Bindable<RecruitmentModel.Cell>(RecruitmentModel.mockCell_1)
+        }
+        // 募集詳細取得アクション
+        store.send(.getRecruitmentDetail)
+        // 受け取るアクション
+        store.receive(.responseRecruitmentDetail(.failure(APIError.serverError))) { response in
+            response.errorStatus = Bindable<String>(APIError.serverError.localize)
+        }
+    }
 }
